5c5e425c9f8fd3d4858bf2c19125be8cbda03322
Lynnmich <lynnenmichuki@gmail.com>
2024-06-26 09:35:02 +0300

    Third commit

--- weather-app/src/app/weather.service.ts
+++ weather-app/src/app/weather.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  private apiKey = '6e5dc98de2dbf14c7aea6d23e6292102';
-  private apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&units=metric&q=`;
+  private apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${environment.weatherApiKey}&q=`;
 
   constructor(private http: HttpClient) { }
 
